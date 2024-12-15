39610acde57c4c5dad85142b2823340d8ce0f15f
Thais Rodrigues <thais.rl@outlook.com>
2024-12-14 22:29:00 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Digite um número: "))
-dobro = num * 2
-triplo = num * 3
-raiz = num ** (1/2)
-print(f"O dobro de {num} vale {dobro}")
-print(f"O triplo de {num} vale {triplo}")
-print(f"A raiz quadrada de {num} é {raiz:.2f}")
