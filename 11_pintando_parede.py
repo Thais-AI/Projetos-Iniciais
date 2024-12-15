5b9b2be6b462594f71253aed3aca92eae306abaf
Thais Rodrigues <thais.rl@outlook.com>
2024-12-15 19:58:05 -0300

    Create 11_pintando_parede.py

---
+++ 11_pintando_parede.py
@@ -0,0 +1,5 @@
+l = float(input("Qual a largura da parede? "))
+a = float(input("Qual a altura da parede? "))
+area = l * a
+print(f"Sua parede tem a dimensão de {l} x {a} e sua área é de {area}m².")
+print(f"Para pintar sua parede, você precisará de {area/2}l de tinta.")
