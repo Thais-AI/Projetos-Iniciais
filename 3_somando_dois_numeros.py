22f9a190865c6bb2d75d6d7834f8092e449ae00d
Thais Rodrigues <thais.rl@outlook.com>
2024-12-14 22:28:49 -0300

    Create 3_somando_dois_numeros.py

---
+++ 3_somando_dois_numeros.py
@@ -0,0 +1,10 @@
+### PRIMEIRA OPÇÃO
+primeiro = float(input("Digite um valor: "))
+segundo = float(input("Digite outro valor: "))
+print(f"A soma entre {primeiro} e {segundo} é igual a {primeiro+segundo}")
+
+### SEGUNDA OPÇÃO
+primeiro = float(input("Digite um valor: "))
+segundo = float(input("Digite outro valor: "))
+soma = primeiro + segundo
+print(f"A soma entre {primeiro} e {segundo} é igual a {soma}")
